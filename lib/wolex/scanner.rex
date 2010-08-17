6b42fd6cb1b8072751224de858d07d5d935a0a89
Rizwan Reza <rizwanreza@gmail.com>
2010-08-18 04:06:18 +0430

    Basic calculator.

--- lib/wolex/scanner.rex
+++ lib/wolex/scanner.rex
@@ -3,12 +3,19 @@ class Scanner
 
 macro
   number    \d+
-
+  add      \+
+  subtract      \-
+  multiply  \*
+  divide    \/
+  blank     [\ \t]+
 rule
 
 # [:state]  pattern  [actions]
 
             {number}  { [:NUMBER, text.to_i] }
-
+            {add}  { [:ADD, text] }
+            {subtract}  { [:SUBTRACT, text] }
+            {multiply}  { [:MULTIPLY, text] }
+            {divide}  { [:DIVIDE, text] }
 end
 end
--- test/test_scanner.rb
+++ test/test_scanner.rb
@@ -2,10 +2,54 @@ require 'helper'
 
 module Wolex
   describe 'scanner' do
-    it 'should scan stuff' do
+    it 'should scan number' do
       scanner = Wolex::Scanner.new
       scanner.scan_setup '10'
       scanner.next_token.must_equal [:NUMBER, 10]
+    end
+
+    it 'should scan plus sign' do
+      scanner = Wolex::Scanner.new
+      scanner.scan_setup '+'
+      scanner.next_token.must_equal [:ADD, "+"]
+    end
+
+    it 'should scan additive expression' do
+      scanner = Wolex::Scanner.new
+      scanner.scan_setup '1+'
+      scanner.next_token.must_equal [:NUMBER, 1]
+      scanner.next_token.must_equal [:ADD, "+"]
+    end
+
+    it 'should scan subtractive expression' do
+      scanner = Wolex::Scanner.new
+      scanner.scan_setup '1-'
+      scanner.next_token.must_equal [:NUMBER, 1]
+      scanner.next_token.must_equal [:SUBTRACT, "-"]
+    end
+
+    it 'should scan complex expression' do
+      scanner = Wolex::Scanner.new
+      scanner.scan_setup '1-1+3'
+      scanner.next_token.must_equal [:NUMBER, 1]
+      scanner.next_token.must_equal [:SUBTRACT, "-"]
+      scanner.next_token.must_equal [:NUMBER, 1]
+      scanner.next_token.must_equal [:ADD, "+"]
+      scanner.next_token.must_equal [:NUMBER, 3]
+    end
+
+    it 'should scan multiplication sign' do
+      scanner = Wolex::Scanner.new
+      scanner.scan_setup '1*'
+      scanner.next_token.must_equal [:NUMBER, 1]
+      scanner.next_token.must_equal [:MULTIPLY, "*"]
+    end
+
+    it 'should scan division sign' do
+      scanner = Wolex::Scanner.new
+      scanner.scan_setup '1/'
+      scanner.next_token.must_equal [:NUMBER, 1]
+      scanner.next_token.must_equal [:DIVIDE, "/"]
     end
   end
 end
